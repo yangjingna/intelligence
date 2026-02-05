bb8c39314ed860d17bdfd9c5cff6ae521c3b44fe
yangjingna <2281792977@qq.com>
2026-02-05 13:07:28 +0800

    更新build

--- react_demo/src/services/api.js
+++ react_demo/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // 开发环境
-const API_BASE_URL = 'http://47.108.117.7:8000';
+const API_BASE_URL = 'http://47.108.117.7:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
